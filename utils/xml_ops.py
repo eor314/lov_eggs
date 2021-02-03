36cd69ad7aece515f869243731b9a4948a650d74
eor314 <e1orenst@ucsd.edu>
2021-02-03 18:03:05 +0100

    added iteration to populate_voc. accepts multiple labels and bounding boxes

--- utils/xml_ops.py
+++ utils/xml_ops.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import copy
 import glob
 import os
 from make_mosaic_psd import get_dim
@@ -10,7 +11,7 @@ def populate_voc(template, outdir, imgptf, bbox, label):
     :param template: xml template to work from
     :param outdir: path to output directory (where new files are copied)
     :param imgptf: where the image lives
-    :param bbox: bounding box coordinates [list of tuples]
+    :param bbox: bounding box coordinates [array, each row corresponds to label]
     :param label: labels corresponding to bboxes and segments [list of ind]
     """
 
@@ -30,26 +31,43 @@ def populate_voc(template, outdir, imgptf, bbox, label):
     fname = bs_data.filename
     fname.string = os.path.split(imgptf)[1]
 
-    # get the dimensions
+    # get the whole dimensions
     wd, ht = get_dim(imgptf)
     width = bs_data.size.width
     width.string = str(wd)
     height = bs_data.size.height
     height.string = str(ht)
 
+    # copy the object tag for however many bounding boxes are in the ROI
+    flag = 1
+    while flag < bbox.shape[0]:
+        bs_data.annotation.append(copy.copy(bs_data.object))
+        flag += 1
+
+    # select all empty elements in the xml document
+    nns = bs_data.select('name:empty')  # list of empty name tags
+    xmins = bs_data.select('xmin:empty')
+    ymins = bs_data.select('ymin:empty')
+    xmaxs = bs_data.select('xmax:empty')
+    ymaxs = bs_data.select('ymax:empty')
+
     # bounding box info
     # for now just select the first tuple and hardcode other into [012221]
-    bb = bbox[0]
-    name = bs_data.object.find('name')  # have to use find since 'name' heading shadows bs4 method
-    name.string = label[0]
-    xmin = bs_data.object.bndbox.xmin
-    xmin.string = str(bb[1])
-    ymin = bs_data.object.bndbox.ymin
-    ymin.string = str(bb[0])
-    xmax = bs_data.object.bndbox.xmax
-    xmax.string = str(bb[3])
-    ymax = bs_data.object.bndbox.ymax
-    ymax.string = str(bb[2])
+    for ii in range(bbox.shape[0]):
+        # enter the label string
+        name = nns[ii]
+        name.string = label[ii]
+
+        # enter the corresponding bbox location
+        bb = bbox[ii, ::]
+        xmin = xmins[ii]
+        xmin.string = str(bb[1])
+        ymin = ymins[ii]
+        ymin.string = str(bb[0])
+        xmax = xmaxs[ii]
+        xmax.string = str(bb[3])
+        ymax = ymaxs[ii]
+        ymax.string = str(bb[2])
 
     outpath = os.path.join(outdir, os.path.splitext(os.path.basename(imgptf))[0]+'.xml')
 
